4fd2dc189752e5c5219b0fd943abcb8ee3596411
manhcuong <manhcuong@example.com>
2019-05-16 21:18:47 +0700

    Update docker file

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM ruby:2.5.3
+RUN apt-get update -qq && apt-get install -y nodejs postgresql-client
+RUN mkdir /depot
+WORKDIR /depot
+COPY Gemfile /depot/Gemfile
+COPY Gemfile.lock /depot/Gemfile.lock
+ENV BUNDLER_VERSION 2.0.1
+RUN gem install bundler && bundle install --jobs 20 --retry 5
+COPY . /depot
+
+COPY entrypoint.sh /usr/bin/
+RUN chmod +x /usr/bin/entrypoint.sh
+ENTRYPOINT ["entrypoint.sh"]
+EXPOSE 3000
+
+CMD ["rails", "server", "-b", "0.0.0.0"]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
+                description:
+                    %{<p>
+                      <em>Native Apps, Multiple Platforms</em>
+                      Answer the question “Can we build this for ALL the devices?” with a
+                      resounding YES. This book will help you get there with a real-world
+                      introduction to seven platforms, whether you’re new to mobile or an
+                      experienced developer needing to expand your options. Plus, you’ll find
+                      out which cross-platform solution makes the most sense for your needs.
+                      </p>},
+                image_url: 'mutest.jpg',
+                price: 26.00)
