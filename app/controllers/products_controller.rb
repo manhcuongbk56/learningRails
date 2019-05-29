15ab2ebf62976ca9e661a17b87e728949bc05a18
Chau Hong Linh <chauhonglinh@gmail.com>
2019-05-28 21:10:40 -0500

    Add product_service. Reorganize products_controller #create and #update.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,31 +24,30 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    @product = Product.new(product_params)
+    options = {
+      html_notice: 'Product was successfully created.',
+      json_render: :show,
+      json_status: :created,
+      html_error_action: :new
+    }
 
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        # puts @product.errors.full_messages
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    perform_action(options) do
+      @product = ProductService.create_product!(product_params)
     end
   end
 
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
   def update
-    respond_to do |format|
-      if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    options = {
+      html_notice: 'Product was successfully updated.',
+      json_render: :show,
+      json_status: :ok,
+      html_error_action: :edit
+    }
+    
+    perform_action(options) do
+      @product = ProductService.update_product!(@product, product_params)
     end
   end
 
@@ -63,13 +62,29 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
+  
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:title, :description, :image_url, :price)
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:title, :description, :image_url, :price)
+  end
+
+  def perform_action(options)
+    yield
+
+    respond_to do |format|
+      format.html { redirect_to @product, notice: options[:html_notice] }
+      format.json { render options[:json_render], status: options[:json_status], location: @product }
     end
+  rescue ActiveRecord::RecordInvalid => error
+    Rails.logger.error("#{error.message}\n#{error.backtrace.join("\n")}")
+    respond_to do |format|
+      format.html { render options[:html_error_action] }
+      format.json { render json: @product.errors, status: :unprocessable_entity }
+    end
+  end
 end
